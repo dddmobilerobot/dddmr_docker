6d45cc045ab11df430ddfc0896d7b3dc7dce8fa9
apola <yan_1whun1@hotmail.com>
2024-07-14 15:11:07 +0400

    cuda bash

--- docker_file/build.bash
+++ docker_file/build.bash
@@ -5,10 +5,17 @@ echo -n "Select image type (x64/l4t): "
 read image_type
 
 if [ "$image_type" != "${image_type#[x64]}" ] ;then 
-    echo "----> Creating x64 image"
-    docker build -t dddmr_gtsam:x64 -f Dockerfile_gtsam .
+    echo -n "Do you want to build image using cuda? (Y/N): " 
+    read is_cuda
+    if [ "$is_cuda" != "${is_cuda#[Yy]}" ] ;then 
+        echo "----> Creating x64 image with cuda"
+        docker build -t dddmr_gtsam:12.4.1-cudnn-devel-ubuntu22.04 -f Dockerfile_gtsam_cuda .
+    else
+        echo "----> Creating x64 image without cuda"
+        docker build -t dddmr_gtsam:x64 -f Dockerfile_gtsam .
+    fi
 else
     echo "----> Creating l4t image"
-    docker build -t dddmr_gtsam:l4t_r36.3 -f Dockerfile_gtsam_l4t_r36 .
+    docker build -t dddmr_gtsam:l4t_r36.2 -f Dockerfile_gtsam_l4t_r36 .
 fi
 
