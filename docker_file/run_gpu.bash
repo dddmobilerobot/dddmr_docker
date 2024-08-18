b22748a6af5ed199abc665d1cefa4b9d54010359
apola <yan_1whun1@hotmail.com>
2024-08-18 18:42:36 +0400

    update cuda

--- docker_file/run_gpu.bash
+++ docker_file/run_gpu.bash
@@ -13,5 +13,5 @@ docker run -it \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --volume="${HOME}/dddmr:/root/dddmr_navigation" \
     --volume="${HOME}/dddmr_bags:/root/dddmr_bags" \
-    --name="dddmr_ros2_gpu_dev2" \
-    dddmr_gtsam:x64
+    --name="dddmr_ros2_gpu_dev" \
+    dddmr_gtsam:12.4.1-cudnn-devel-ubuntu22.04
